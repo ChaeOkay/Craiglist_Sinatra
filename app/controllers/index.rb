818ee325b5260cfc6de782f7c8946899d527a4c8
cokeefe <chaemobile@gmail.com>
2013-08-06 16:38:59 -0700

    outline routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,11 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+#action index - displays all categories
+get '/categories' do
+end
+
+#action index - displays all posts within a category
+get '/categories/posts' do
+end
