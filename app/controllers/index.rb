6faf8236bac6d3414ddeeed8330fa5b7110ab382
cokeefe <chaemobile@gmail.com>
2013-08-06 17:32:01 -0700

    redirect to posts

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,9 +6,12 @@ end
 #action index - displays all categories
 get '/categories' do
   @categories = Category.all
+  @selected_category = params[:categories]
   erb :index
 end
 
 #action index - displays all posts within a category
-get '/categories/posts' do
+get '/categories/:posts' do
+  p @posts = params[:posts]
+  erb :category_posts_index
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,25 @@
 
 Category.create(description: "Apt/Housing")
 Category.create(description: "Rooms/Shared")
-Category.create(description: "Sublet/Temporary")
-
-# Post.create( title: Faker::Name.name,
-#                   description: Faker::Company.bs,
-#                   email: Faker::Internet.email,
-#                   rent: rand(700..200),
-#                   area: rand(600..1200) )
 
 Post.create( {title: "Your new home in Chinatown",
                   description: "Studio, new appliances, w/d",
-                  email: "guest@guest.com",
+                  email: "guest1@guest.com",
                   rent: 2000,
                   area: 650 } )
+
+Post.create( {title: "Your new home in Fisherman's Warf",
+                  description: "1bd, new appliances, w/d",
+                  email: "guest2@guest.com",
+                  rent: 1800,
+                  area: 600 } )
+
+Post.create( {title: "Your new home in Russian Hill",
+                  description: "2bd, new appliances, w/d",
+                  email: "guest3@guest.com",
+                  rent: 2200,
+                  area: 500 } )
+
+c = Category.first
+c.posts << Post.all
+
