8eec06e1ffc02fb746bb00341c5d2647208456b8
cokeefe <chaemobile@gmail.com>
2013-08-06 20:57:45 -0700

    add key

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -24,3 +24,28 @@ get '/categories/posts/:post_id' do
   @category = @post.category.description
   erb :show_post
 end
+
+#action new - return an HTML form for creating a new post
+get '/categories/:category/posts/new' do
+  @category = params[:category]
+  erb :create_post
+end
+
+#creates a new post in the database
+post '/categories/:category/posts/new' do
+  new_post = Post.create( {title: params[:title],
+                  description: params[:description],
+                  email: params[:email],
+                  secure_email: "fjf8932983hiufewf28y9@craigslistjr.com",
+                  rent: params[:rent],
+                  area: params[:area],
+                  key: "wzdzihu7832" } )
+  category = Category.where(:description => params[:category]).first
+  @post = Post.where(:title => params[:title])
+  category.posts << @post
+  redirect "/categories/#{category.id}"
+end
+
+get '/categories/posts/:post_id/:key' do
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,36 @@
 # require 'faker'
 
-Category.create(description: "Apt/Housing")
-Category.create(description: "Rooms/Shared")
+Category.create(description: "Apartments")
+Category.create(description: "Rooms-Shared")
 
 Post.create( {title: "Your new home in Chinatown",
                   description: "Studio, new appliances, w/d",
                   email: "guest1@guest.com",
                   secure_email: "2odfoi4n289r32@craigslistjr.com",
                   rent: 2000,
-                  area: 650 } )
+                  area: 650,
+                  key: "f32983udj9" } )
 
 Post.create( {title: "Your new home in Fisherman's Warf",
                   description: "1bd, new appliances, w/d",
                   email: "guest2@guest.com",
                   secure_email: "fjf8932983hiufewf28y9@craigslistjr.com",
                   rent: 1800,
-                  area: 600 } )
+                  area: 600,
+                  key: "f9rh9298rhh" } )
 
 Post.create( {title: "Your new home in Russian Hill",
                   description: "2bd, new appliances, w/d",
                   email: "guest3@guest.com",
                   secure_email: "8923982h982roy8q@craigslistjr.com",
                   rent: 2200,
-                  area: 500 } )
+                  area: 500,
+                  key: "9hrh2h2h32r" } )
 
-c = Category.first
-c.posts << Post.all
+c_first = Category.first
+c_first.posts << Post.find(1)
+c_first.posts << Post.find(2)
+
+c_last = Category.last
+c_last.posts << Post.find(3)
 
