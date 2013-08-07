da7128f5696e40756ccc07e72d3ddbb0c6de3b41
cokeefe <chaemobile@gmail.com>
2013-08-06 18:09:09 -0700

    show_post view

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,11 +7,20 @@ end
 get '/categories' do
   @categories = Category.all
   @selected_category = params[:categories]
-  erb :index
+  erb :index_categories
 end
 
 #action index - displays all posts within a category
 get '/categories/:category_id' do
-  @category = Category.find(params[:category_id]).description
-  erb :category_posts_index
+  category = Category.find(params[:category_id])
+  @category = category.description
+  @posts = category.posts
+  erb :index_categories_posts
+end
+
+#action show - displays a specific post
+get '/categories/posts/:post_id' do
+  @post = Post.find(params[:post_id])
+  @category = @post.category.description
+  erb :show_post
 end
